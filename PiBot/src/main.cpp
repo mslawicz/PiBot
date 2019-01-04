72aa5a42236c164935554a58daf8004f2f8dc11b
Marcin <slawicz@echostar.pl>
2019-01-04 23:24:17 +0100

    main.cpp created main() function created

---
+++ PiBot/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 4 sty 2019
+ *      Author: Marcin
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello from PiBot!!!" << std::endl;
+	return 0;
+}
+
