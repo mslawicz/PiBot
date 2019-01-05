44fa06af5460255617aa908f450b7b067ded4492
Marcin <slawicz@echostar.pl>
2019-01-05 01:10:15 +0100

    pigpio library successfully linked it requires launching program using root account

--- PiBot/src/main.cpp
+++ PiBot/src/main.cpp
@@ -6,9 +6,15 @@
  */
 
 #include <iostream>
+#include <pigpio.h>
 
 int main(int argc, char* argv[])
 {
+	if (gpioInitialise() < 0)
+	   {
+	      fprintf(stderr, "pigpio initialisation failed\n");
+	      return 1;
+	   }
 	std::cout << "Hello from PiBot!!!" << std::endl;
 	return 0;
 }
