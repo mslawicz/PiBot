ef902a2ba35de42bcf94a56526b55b7e78ad9704
Marcin <slawicz@echostar.pl>
2019-01-05 17:01:40 +0100

    pigpio successfully tested

--- PiBot/src/main.cpp
+++ PiBot/src/main.cpp
@@ -15,7 +15,18 @@ int main(int argc, char* argv[])
 	      fprintf(stderr, "pigpio initialisation failed\n");
 	      return 1;
 	   }
-	std::cout << "Hello from PiBot!!!" << std::endl;
+	std::cout << "Hello from PiBot!" << std::endl;
+	auto res = gpioSetMode(17, PI_OUTPUT);
+	std::cout << "set pin 17: " << res << std::endl;
+	std::cout << "gpio hardware revision: " << gpioHardwareRevision() << std::endl;
+	gpioWrite(17, 1);
+	gpioDelay(200000);
+	gpioWrite(17, 0);
+	gpioDelay(200000);
+	gpioWrite(17, 1);
+	gpioDelay(200000);
+	gpioWrite(17, 0);
+	gpioTerminate();
 	return 0;
 }
 
