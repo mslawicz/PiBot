52ade8dff7fc0c5441fb8f7acc5fba97fcf315c9
Marcin <slawicz@echostar.pl>
2019-01-05 22:42:05 +0100

    test of launching another thread (pigpio library)

--- PiBot/src/main.cpp
+++ PiBot/src/main.cpp
@@ -8,6 +8,15 @@
 #include <iostream>
 #include "gpio.h"
 
+void* myFunction(void* arg)
+{
+	static int counter = 0;
+	while(1)
+	{
+		std::cout << (char*)arg << " #" << counter++ << std::endl;
+		gpioDelay(1000000);
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -16,6 +25,8 @@ int main(int argc, char* argv[])
 	std::cout << "gpio hardware revision: " << gpioHardwareRevision() << std::endl;
 	GPIO GreenLED(17, PI_OUTPUT);
 	GPIO UserKey(21, PI_INPUT, PI_PUD_UP);
+	pthread_t* pth;
+	pth = gpioStartThread(myFunction, (void*)"another thread: loop");
 
 	while(UserKey.Read())
 	{
@@ -23,6 +34,7 @@ int main(int argc, char* argv[])
 		gpioDelay(200000);
 	}
 
+	gpioStopThread(pth);
 	std::cout << "Good bye!" << std::endl;
 	GPIO::Terminate();
 	return 0;
