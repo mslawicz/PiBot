f9c1b0e7d3085507f49d3e0ab9108f0b876c2d47
Marcin <slawicz@echostar.pl>
2019-01-12 23:13:39 +0100

    test of gpioSetGetSamplesFunc() and its related callback
    samples are being printed out

--- PiBot/src/main.cpp
+++ PiBot/src/main.cpp
@@ -11,6 +11,32 @@
 #include "logger.h"
 #include <iostream>
 
+GPIO* pResult;
+uint32_t g_bitMask;
+
+void samplingFunc(const gpioSample_t* sampleArray, int noOfSamples)
+{
+	static unsigned int counter = 0;
+	static int lastTick = 0;
+	static uint32_t lastLevel = 0;
+
+	pResult->toggle();
+	counter++;
+	if((counter % 1000) <= 1)
+	{
+		std::cout << noOfSamples << ": ";
+		for(int k=0; k<noOfSamples; k++)
+		{
+			std::cout << std::dec << std::setw(11) << sampleArray[k].tick - lastTick << ",";
+			std::cout << "0x" << std::setw(8) << std::hex << std::setfill(' ') << (sampleArray[k].level ^ lastLevel) << "\t";
+			lastTick = sampleArray[k].tick;
+			lastLevel = sampleArray[k].level;
+		}
+
+		std::cout << std::endl;
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	Logger::getInstance().logEvent(INFO, "PiBot started");
@@ -19,39 +45,31 @@ int main(int argc, char* argv[])
 
 
 	std::cout << "gpio hardware revision: " << gpioHardwareRevision() << std::endl;
-	GPIO GreenLED(17, PI_OUTPUT);
-	GPIO UserKey(21, PI_INPUT, PI_PUD_UP);
+	GPIO greenLED(17, PI_OUTPUT);
+	GPIO userKey(21, PI_INPUT, PI_PUD_UP);
 
 	gpioSetPullUpDown(2, PI_PUD_UP);
 	gpioSetPullUpDown(3, PI_PUD_UP);
 	I2C Gyroscope(I2C1, 0x6B);		// LSM9DS1 - Accelerometer and gyroscope
 	I2C Magnetometer(I2C1, 0x1E);	// LSM9DS1 - Magnetic sensor
 
-	auto Data = Gyroscope.read(0x15, 9);
-	std::cout << "the length of Data vector: " << Data.size() << std::endl;
-	for(auto Byte : Data)
+	pResult = new GPIO(5, PI_INPUT);
+
+	GPIO testPWM(6, PI_OUTPUT);
+	gpioSetPWMfrequency(6, 1600);
+	gpioPWM(6, 64);
+	GPIO samplePort(12, PI_INPUT);
+	g_bitMask = 1 << 12;
+	gpioSetGetSamplesFunc(samplingFunc, g_bitMask);
+
+	while (userKey.read())
 	{
-		std::cout << std::hex << (int)Byte << ",";
+
 	}
-	std::cout << std::endl;
-
-	// the time between two devices readout is 0.14 ms
-	Data.clear();
-	Data.push_back(0xBA);
-	Data.push_back(0x40);
-	Magnetometer.write(0x20, Data);
-	Data = Magnetometer.read(0x20, 5);
-	std::cout << "the length of Data vector: " << Data.size() << std::endl;
-	for(auto Byte : Data)
-	{
-		std::cout << std::hex << (int)Byte << ",";
-	}
-	std::cout << std::endl;
-	gpioDelay(1000);
 
 
 
-
+	delete pResult;
 	Program::getInstance().terminate();
 	return 0;
 }
