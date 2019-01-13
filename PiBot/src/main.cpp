296dd79a0efd35d26b0cbb454eb848802fff63b8
Marcin <slawicz@echostar.pl>
2019-01-13 01:09:51 +0100

    gpioSetGetSamplesFunc() fully tested
    TODO: check if object methods can be involved

--- PiBot/src/main.cpp
+++ PiBot/src/main.cpp
@@ -14,6 +14,7 @@
 GPIO* pResult;
 uint32_t g_bitMask;
 
+// this function is called every 1 ms
 void samplingFunc(const gpioSample_t* sampleArray, int noOfSamples)
 {
 	static unsigned int counter = 0;
@@ -22,14 +23,24 @@ void samplingFunc(const gpioSample_t* sampleArray, int noOfSamples)
 
 	pResult->toggle();
 	counter++;
-	if((counter % 1000) <= 1)
+	// this condition is for printing only from time to time
+	if((counter % 1000) <= 2)
 	{
-		std::cout << noOfSamples << ": ";
+		// let's check all samples stored
 		for(int k=0; k<noOfSamples; k++)
 		{
-			std::cout << std::dec << std::setw(11) << sampleArray[k].tick - lastTick << ",";
-			std::cout << "0x" << std::setw(8) << std::hex << std::setfill(' ') << (sampleArray[k].level ^ lastLevel) << "\t";
-			lastTick = sampleArray[k].tick;
+			uint32_t currentLevel = sampleArray[k].level;
+			// filter only rising edge of encoder bit
+			if(((currentLevel ^ lastLevel) & g_bitMask) & currentLevel)
+			{
+				// if encoder bit changes 0->1
+				// then print the number of ticks (us) elapsed since the previous low-to-high edge
+				std::cout << std::dec << std::setw(11) << sampleArray[k].tick - lastTick << ",";
+				// and remember the current tick state
+				lastTick = sampleArray[k].tick;
+
+			}
+			// remember the level of every sample to be able to recognize a rising edge
 			lastLevel = sampleArray[k].level;
 		}
 
