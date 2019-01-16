46fb8f62c88102b5ec9ecd98089adcb0db63a106
Marcin <slawicz@echostar.pl>
2019-01-16 18:02:52 +0100

    Drive files created

---
+++ PiBot/src/Drive.cpp
@@ -0,0 +1,15 @@
+/*
+ * Drive.cpp
+ *
+ *  Created on: 16 sty 2019
+ *      Author: Marcin
+ */
+
+#include "Drive.h"
+
+Drive::Drive()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ PiBot/src/Drive.h
@@ -0,0 +1,17 @@
+/*
+ * Drive.h
+ *
+ *  Created on: 16 sty 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_DRIVE_H_
+#define SRC_DRIVE_H_
+
+class Drive
+{
+public:
+	Drive();
+};
+
+#endif /* SRC_DRIVE_H_ */
