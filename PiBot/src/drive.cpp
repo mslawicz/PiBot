50a8459be19040ebb2489790d43555b5afc1108f
Marcin <slawicz@echostar.pl>
2019-01-17 23:20:24 +0100

    IMU files added
    IMU class reated
    lots of refactoring
    rebuild indexer

--- PiBot/src/drive.cpp
+++ PiBot/src/drive.cpp
@@ -6,22 +6,10 @@
  */
 
 #include "drive.h"
-#include "LSM9DS1.h"
-#include "logger.h"
-#include "program.h"
 
 Drive::Drive()
 {
-	pGyroInterruptPort = new GPIO(13, PI_INPUT, PI_PUD_UP);
-	// interface to LSM9DS1 - Accelerometer and gyroscope
-	pIMUInterface = new I2C(I2C1, 0x6B);
-	// verify the IMU device
-	auto data = pIMUInterface->read(INU_Registers::WHO_AM_I, 1);
-	if (data[0] != LSM9DS1_AG)
-	{
-		// this is not expected LSM9DS1 A/G device
-		Program::getInstance().terminate(WRONG_I2C_DEVICE);
-	}
+	pIMU = new IMU;
 
 	pGreenLED = new GPIO(23, PI_OUTPUT);	//XXX test
 	pGreenLED->write(1);	//XXX test
@@ -29,8 +17,7 @@ Drive::Drive()
 
 Drive::~Drive()
 {
-	delete pIMUInterface;
-	delete pGyroInterruptPort;
+	delete pIMU;
 
 	pGreenLED->write(0);	//XXX test
 	delete pGreenLED;	//XXX test
@@ -41,8 +28,9 @@ Drive::~Drive()
  */
 void Drive::start(void)
 {
-	// enable giroscope interrupts
-	gpioSetISRFuncEx(pGyroInterruptPort->getNumber(), FALLING_EDGE, 0, Drive::gyroInterruptCallback, this);
+	// enable gyroscope interrupts
+	gpioSetISRFuncEx(pIMU->getGyroInterruptPort(), FALLING_EDGE, 0,
+			Drive::gyroInterruptCallback, this);
 }
 
 /*
@@ -50,15 +38,17 @@ void Drive::start(void)
  */
 void Drive::stop(void)
 {
-	// disable giroscope interrupts
-	gpioSetISRFuncEx(pGyroInterruptPort->getNumber(), FALLING_EDGE, 0, nullptr, this);
+	// disable gyroscope interrupts
+	gpioSetISRFuncEx(pIMU->getGyroInterruptPort(), FALLING_EDGE, 0, nullptr,
+			this);
 }
 
 /*
  * callback function for gyroscope generated interrupts
  * it must be a static method, but the pointer to drive object is passed as an argument
  */
-void Drive::gyroInterruptCallback(int gpio, int level, uint32_t tick, void* pDriveObject)
+void Drive::gyroInterruptCallback(int gpio, int level, uint32_t tick,
+		void* pDriveObject)
 {
 	static_cast<Drive*>(pDriveObject)->pitchControl(level, tick);
 
---
+++ PiBot/src/imu.cpp
@@ -0,0 +1,33 @@
+/*
+ * imu.cpp
+ *
+ *  Created on: 17 sty 2019
+ *      Author: Marcin
+ */
+
+#include "imu.h"
+#include "LSM9DS1.h"
+#include "program.h"
+#include "logger.h"
+
+IMU::IMU()
+{
+	// port of gyroscope interrupt signal
+	pGyroInterruptPort = new GPIO(13, PI_INPUT, PI_PUD_UP);
+	// interface to IMU device
+	pInterface = new I2C(I2C1, 0x6B);
+	// verify the IMU device
+	auto data = pInterface->read(IMU_Registers::WHO_AM_I, 1);
+	if (data[0] != LSM9DS1_AG)
+	{
+		// this is not expected LSM9DS1 A/G device
+		Program::getInstance().terminate(WRONG_I2C_DEVICE);
+	}
+}
+
+IMU::~IMU()
+{
+	delete pInterface;
+	delete pGyroInterruptPort;
+}
+
