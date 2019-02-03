a4bbb80868549e548251c04b4ae50dfc1e8498f1
Marcin <slawicz@echostar.pl>
2019-02-03 17:26:06 +0100

    motor files created

---
+++ PiBot/src/motor.cpp
@@ -0,0 +1,20 @@
+/*
+ * motor.cpp
+ *
+ *  Created on: 3 lut 2019
+ *      Author: Marcin
+ */
+
+#include "motor.h"
+
+Motor::Motor()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Motor::~Motor()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ PiBot/src/motor.h
@@ -0,0 +1,18 @@
+/*
+ * motor.h
+ *
+ *  Created on: 3 lut 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_MOTOR_H_
+#define SRC_MOTOR_H_
+
+class Motor
+{
+public:
+    Motor();
+    ~Motor();
+};
+
+#endif /* SRC_MOTOR_H_ */
