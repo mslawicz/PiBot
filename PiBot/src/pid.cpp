4ed047d3e8180db976fd5863c17cda0d0702c6f0
Marcin <slawicz@echostar.pl>
2019-02-11 19:22:24 +0100

    PID files added

---
+++ PiBot/src/pid.cpp
@@ -0,0 +1,15 @@
+/*
+ * pid.cpp
+ *
+ *  Created on: 11 lut 2019
+ *      Author: Marcin
+ */
+
+#include "pid.h"
+
+PID::PID()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ PiBot/src/pid.h
@@ -0,0 +1,17 @@
+/*
+ * pid.h
+ *
+ *  Created on: 11 lut 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_PID_H_
+#define SRC_PID_H_
+
+class PID
+{
+public:
+    PID();
+};
+
+#endif /* SRC_PID_H_ */
