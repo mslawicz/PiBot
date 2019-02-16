2d57a62a57a3f5d97b3397c3bbb186fd35d662d3
Marcin <slawicz@echostar.pl>
2019-02-16 09:24:20 +0100

    serial files added

---
+++ PiBot/src/serial.cpp
@@ -0,0 +1,20 @@
+/*
+ * serial.cpp
+ *
+ *  Created on: 16 lut 2019
+ *      Author: Marcin
+ */
+
+#include "serial.h"
+
+Serial::Serial()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Serial::~Serial()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ PiBot/src/serial.h
@@ -0,0 +1,18 @@
+/*
+ * serial.h
+ *
+ *  Created on: 16 lut 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_SERIAL_H_
+#define SRC_SERIAL_H_
+
+class Serial
+{
+public:
+    Serial();
+    ~Serial();
+};
+
+#endif /* SRC_SERIAL_H_ */
