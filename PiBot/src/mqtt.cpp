9fae34f430e4d9a327198125fc46980d88046823
Marcin <slawicz@echostar.pl>
2019-03-02 17:18:37 +0100

    mqtt files added; mosquitto library applied

---
+++ PiBot/src/mqtt.cpp
@@ -0,0 +1,20 @@
+/*
+ * mqtt.cpp
+ *
+ *  Created on: 2 mar 2019
+ *      Author: Marcin
+ */
+
+#include "mqtt.h"
+
+MQTT::MQTT()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+MQTT::~MQTT()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ PiBot/src/mqtt.h
@@ -0,0 +1,20 @@
+/*
+ * mqtt.h
+ *
+ *  Created on: 2 mar 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_MQTT_H_
+#define SRC_MQTT_H_
+
+#include <mosquittopp.h>
+
+class MQTT
+{
+public:
+    MQTT();
+    ~MQTT();
+};
+
+#endif /* SRC_MQTT_H_ */
