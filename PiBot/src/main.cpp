030b8074a05d78d5d250d58d07494f7749b59194
Marcin <slawicz@echostar.pl>
2019-03-16 10:52:30 +0100

    Config::updateLanInterfaces function created

--- PiBot/src/main.cpp
+++ PiBot/src/main.cpp
@@ -7,9 +7,6 @@
 
 #include "program.h"
 #include "logger.h"
-#include <ifaddrs.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <iostream>
 #include <chrono>
 
@@ -21,35 +18,6 @@ int main(int argc, char* argv[])
 
 	{
 	    // scope of test objects
-	    const int MaxHost = 20;
-	    char host[MaxHost];
-	    struct ifaddrs* pIfAddr = nullptr;
-	    getifaddrs(&pIfAddr);
-	    struct ifaddrs* pIfAddrCp = pIfAddr;
-	    while(pIfAddr != nullptr)
-	    {
-	        if(pIfAddr->ifa_addr == nullptr)
-	        {
-	            continue;
-	        }
-	        std::cout << pIfAddr->ifa_name << ",";
-	        auto family = pIfAddr->ifa_addr->sa_family;
-	        std::cout << family << ",";
-	        if(family == AF_INET)
-	        {
-	            int s = getnameinfo(pIfAddr->ifa_addr, sizeof(struct sockaddr_in), host, MaxHost, NULL, 0, NI_NUMERICHOST);
-	            if(s)
-	            {
-	                std::cout << "  getnameinfo error\n";
-	                continue;
-	            }
-	            std::cout << host;
-	        }
-	        std::cout << std::endl;
-	        pIfAddr = pIfAddr->ifa_next;
-	    }
-	    freeifaddrs(pIfAddrCp);
-
 
 	    while(!Program::getInstance().isExitRequest())
 	    {
