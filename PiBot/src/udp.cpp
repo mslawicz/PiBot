f42c061774732e97481aa27bef3ac1b3c2b3bd2e
Marcin <slawicz@echostar.pl>
2019-03-21 18:25:53 +0100

    UDP files and namespace created

---
+++ PiBot/src/udp.cpp
@@ -0,0 +1,24 @@
+/*
+ * udp.cpp
+ *
+ *  Created on: 21 mar 2019
+ *      Author: Marcin
+ */
+
+#include "udp.h"
+
+namespace UDP
+{
+
+Client::Client()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Client::~Client()
+{
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace UDP */
---
+++ PiBot/src/udp.h
@@ -0,0 +1,23 @@
+/*
+ * udp.h
+ *
+ *  Created on: 21 mar 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_UDP_H_
+#define SRC_UDP_H_
+
+namespace UDP
+{
+
+class Client
+{
+public:
+    Client();
+    ~Client();
+};
+
+} /* namespace UDP */
+
+#endif /* SRC_UDP_H_ */
