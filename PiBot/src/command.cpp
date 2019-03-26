ae832c2242d0292c6bff6816a9152ffd0c0249dc
Marcin <slawicz@echostar.pl>
2019-03-26 19:16:12 +0100

    command files added

---
+++ PiBot/src/command.cpp
@@ -0,0 +1,15 @@
+/*
+ * command.cpp
+ *
+ *  Created on: 26 mar 2019
+ *      Author: Marcin
+ */
+
+#include "command.h"
+
+Command::Command()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ PiBot/src/command.h
@@ -0,0 +1,17 @@
+/*
+ * command.h
+ *
+ *  Created on: 26 mar 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_COMMAND_H_
+#define SRC_COMMAND_H_
+
+class Command
+{
+public:
+    Command();
+};
+
+#endif /* SRC_COMMAND_H_ */
