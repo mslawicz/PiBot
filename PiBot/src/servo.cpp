b06b02eff5f7f64ccfef062d91788e67a73d447b
Marcin <slawicz@echostar.pl>
2019-03-29 19:12:43 +0100

    servo files created

---
+++ PiBot/src/servo.cpp
@@ -0,0 +1,15 @@
+/*
+ * servo.cpp
+ *
+ *  Created on: 29 mar 2019
+ *      Author: Marcin
+ */
+
+#include "servo.h"
+
+Servo::Servo()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ PiBot/src/servo.h
@@ -0,0 +1,17 @@
+/*
+ * servo.h
+ *
+ *  Created on: 29 mar 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_SERVO_H_
+#define SRC_SERVO_H_
+
+class Servo
+{
+public:
+    Servo();
+};
+
+#endif /* SRC_SERVO_H_ */
