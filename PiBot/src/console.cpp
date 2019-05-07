fe0c0276befa19d26cd6734c68ad5cf0d5d54cee
Marcin <slawicz@echostar.pl>
2019-05-07 22:06:23 +0200

    console corected (Eclipse console is not behaving properly)

--- PiBot/src/console.cpp
+++ PiBot/src/console.cpp
@@ -31,17 +31,17 @@ void Console::handler(void)
     std::string commandLine;
     CLI cli(HostProcess::CONSOLE);
     Logger::getInstance().logEvent(INFO, "Console handler started");
-    std::cin.ignore();
+    //std::cin.ignore();
     std::cin.clear();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     do
     {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         std::cout << '>';
         std::this_thread::yield();
         std::getline(std::cin, commandLine);
         cli.process(commandLine);
-        std::cin.ignore();
+        //std::cin.ignore(255, '\n');
         std::cin.clear();
     } while (!Program::getInstance().isExitRequest());
 
