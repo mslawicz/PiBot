fce461c629d9707779a8bccea8643887b72b8b55
Marcin <slawicz@echostar.pl>
2019-05-11 11:54:25 +0200

    EMA filter added

---
+++ PiBot/src/filter.cpp
@@ -0,0 +1,20 @@
+/*
+ * filter.cpp
+ *
+ *  Created on: 11 maj 2019
+ *      Author: Marcin
+ */
+
+#include "filter.h"
+
+EMA::EMA(float filterStrength)
+    : strength(filterStrength)
+{
+    filteredValue = 0.0f;
+}
+
+float EMA::filter(float input)
+{
+    filteredValue += strength * (input - filteredValue);
+    return filteredValue;
+}
---
+++ PiBot/src/filter.h
@@ -0,0 +1,21 @@
+/*
+ * filter.h
+ *
+ *  Created on: 11 maj 2019
+ *      Author: Marcin
+ */
+
+#ifndef SRC_FILTER_H_
+#define SRC_FILTER_H_
+
+class EMA
+{
+public:
+    EMA(float filterStrength);
+    float filter(float input);
+private:
+    float strength;
+    float filteredValue;
+};
+
+#endif /* SRC_FILTER_H_ */
